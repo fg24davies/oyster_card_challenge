202c06f54baf04ff0f97ead8356075208ad8539e
Flora Davies <daviesfgo@gmail.com>
2021-03-08 15:48:25 +0000

    challenge 5: adding top up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,15 @@
 require 'oystercard'
-
+# challenge 3: I want to my card to have a balance
 describe Oystercard do
   card = Oystercard.new
 
   it 'creates a new card with a balance of 0' do
     expect(card.balance).to eq(0)
   end
+# challenge 5: I want to top up my card
+  it 'tops up the balance of the card' do
+    allow(card).to receive(:balance) { 0 }
+    expect(card.top_up(5)).to eq(5)
+  end
+
 end 
